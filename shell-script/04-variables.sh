a4def35f201dabbf61f372d4fee5d126d381e58a
vandana <vandanareddem685@gmail.com>
2026-01-04 21:03:25 +0530

    fifth

---
+++ shell-script/04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: HEllo$PERSON2,Good Morning"
+echo "$PERSON2: Hi $PERSON1, very Good MOrning"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good,HBU"
