09f1d1e9ff9b92dd27fdc05bf65035622bd07c6e
vandana <vandanareddem685@gmail.com>
2026-01-05 11:39:50 +0530

    USername and password

--- shell-script/05-variables.sh
+++ shell-script/05-variables.sh
@@ -4,4 +4,8 @@ echo "please enter your username"
 
 read -s USERNAME #the value entered above withh be automatically attached to USERNAME variable
 
+echo "please enter your password"
+
+read -s PAASSWORD
+ 
 echo "Username is: $USERNAME, password is: $PASSWORD"
